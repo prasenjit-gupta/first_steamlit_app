e92f0410652c712676c15cf6ee54e0e97342872c
prasenjit-gupta <121782844+prasenjit-gupta@users.noreply.github.com>
2023-01-02 00:21:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 2 & Blueberry Oatmeal')
-streamlit.title('Kale, Spinach and Rocket Smoothie')
-streamlit.title('Har boiled eggs')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Har boiled eggs')
